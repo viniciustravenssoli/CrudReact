17c2bdf99fd3ba88d460a3bb04b09515cba0f205
vts1978 <vinicioussouza@gmail.com>
2022-07-28 07:34:15 -0300

    finish

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default (props) => (
+  <Main icon="home" title="Inicio" subtitle="segundo projeto react">
+    <div className="display-4">Bem vindo</div>
+    <hr />
+    <p className="mb-0">Sistema para exemplificar a construçao de um CRUD 
+        em React</p>
+  </Main>
+);
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido por <i className="fa fa-heart text-danger"></i> por
+            <strong> Vinicius Souza</strong>
+        </span>
+    </footer>
